11b7428ea60959f783c13c954fba9194be2ceac5
ajmar <amarusin18@gmail.com>
2020-11-22 07:38:18 -0700

    commit tree c6f6b248deed9705b3cc6b28827b34d862b9c2ba

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,27 @@
 
 def key_for_min_value(name_hash)
 
+    array = []
+
+    name_hash.each do |key, value|
+
+        array << value
+    end
+
+
+    lowest = array [0]
+
+    array.each do |val|
+        if val < lowest
+            lowest = val
+        end
+    end
+
+    name_hash.each do |key, value|
+        if value == lowest
+            return key
+        end
+    end
+    return nil if name_hash = {}
+
 end
